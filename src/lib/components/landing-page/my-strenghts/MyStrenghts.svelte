8942234d21379ce6204ea706fec6760ae797cbf6
Clément Touzet pc IUT <clement.touzet40@gmail.com>
2023-06-16 07:02:58 +0200

    Mes forces section

---
+++ src/lib/components/landing-page/my-strenghts/MyStrenghts.svelte
@@ -0,0 +1,10 @@
+<script>
+	import ArtisanatStrenght from './ArtisanatStrenght.svelte';
+	import EcoFriendlyStrenght from './EcoFriendlyStrenght.svelte';
+	import PassionStrength from './PassionStrength.svelte';
+</script>
+
+<h2 class="h2">Mes forces</h2>
+<ArtisanatStrenght />
+<EcoFriendlyStrenght />
+<PassionStrength />
