419b7eee4b44d3bea04f73bdc03fcb3a63f27239
Clément Touzet pc IUT <clement.touzet40@gmail.com>
2023-06-20 06:58:58 +0200

    constants for the footer

---
+++ src/lib/utils/constants/routes.ts
@@ -0,0 +1,5 @@
+export const accueilRoute = '/';
+export const blogRoute = '/blog';
+export const infoAndContactRoute = '/info-et-contact';
+export const mesCreationsRoute = '/mes-creations';
+export const severineTouzetRoute = '/severine-touzet';
