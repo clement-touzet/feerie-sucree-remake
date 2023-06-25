20b1e1350078de8fbf8e807585abf50568422657
Clément Touzet pc IUT <clement.touzet40@gmail.com>
2023-06-25 20:25:04 +0200

    catégories des gateaux sur la page d'accueil

---
+++ src/lib/components/landing-page/CakeCategories.svelte
@@ -0,0 +1,13 @@
+<script>
+	import CakeCategorieCard from '../cakes/categories/CakeCategorieCard.svelte';
+</script>
+
+<div class="paddingAndMarginContainer text-center">
+	<h2 class="h2">Choisissez votre type de gâteaux</h2>
+	<div class="no-scrollbar mx-auto mt-4 flex max-w-screen-xl gap-8 overflow-y-auto px-4">
+		<CakeCategorieCard />
+		<CakeCategorieCard />
+		<CakeCategorieCard />
+		<CakeCategorieCard />
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
 	import FeaturesBulletPoints from '$lib/components/severine-touzet/FeaturesBulletPoints.svelte';
 	import HeroSection from '$lib/components/landing-page/HeroSection.svelte';
 	import MyStrenghts from '$lib/components/landing-page/my-strenghts/MyStrenghts.svelte';
+	import CakeCategories from '$lib/components/landing-page/CakeCategories.svelte';
 </script>
 
 <svelte:head>
@@ -11,6 +12,7 @@
 </svelte:head>
 
 <HeroSection />
+<CakeCategories />
 <MyStrenghts />
 <FollowMySocialMedias />
 <CtaFreeQuotation />
